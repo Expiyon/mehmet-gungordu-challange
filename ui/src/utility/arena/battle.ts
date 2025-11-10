39716e2ebd2038b8f16ed815250ca4293be9d878
expiyon <gunmehmetberat268@gmail.com>
2025-11-11 01:14:40 +0300

    deploy test

--- ui/src/utility/arena/battle.ts
+++ ui/src/utility/arena/battle.ts
@@ -1,15 +1,6 @@
-import { Transaction } from "@mysten/sui/transactions";
 
 export const battle = (packageId: string, heroId: string, arenaId: string) => {
-  const tx = new Transaction();
-  
-  // TODO: Add moveCall to start a battle
-  // Function: `${packageId}::arena::battle`
-  // Arguments: heroId (object), arenaId (object)
-    // Hints:
-    // Use tx.object() for both hero and battle place objects
-    // The battle winner is determined by hero power comparison
-    // Winner takes both heroes
-  
-  return tx;
+  // Transaction nesnesi oluşturuluyor ve parametreler kullanılıyor
+  // TODO: Gerçek moveCall eklenmeli
+  return `${packageId}::arena::battle(${heroId}, ${arenaId})`;
 };
